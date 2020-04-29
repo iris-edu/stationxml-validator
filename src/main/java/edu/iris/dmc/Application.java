7b4e82e0658b04a4dbe999700b02a8ace2c4cf7d
timronan <tronan@iris.washington.edu>
2020-04-28 17:20:28 -0700

    Update validator help message.

--- src/main/java/edu/iris/dmc/Application.java
+++ src/main/java/edu/iris/dmc/Application.java
@@ -408,7 +408,7 @@ public class Application {
 		System.out.println("   --ignore-warnings    : don't show warnings");
 		System.out.println("   --rules              : print a list of validation rules");
 		System.out.println("   --units              : print a list of units used to validate");
-		System.out.println("   --verbose            : change the verobsity level to info");
+		System.out.println("   --verbose            : change the verobsity level to info; info is printed to stderr");
 		System.out.println("   --debug              : change the verobsity level to debug");
 		System.out.println("   --help               : print this message");
 		System.out.println("   --continue-on-error  : print exceptions to stdout and processes next file");
